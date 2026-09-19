bf0773a87739dc70533bb2b077fc3b944e4eac4f
KathaFH22 <ct26m033@technikum-wien.at>
2026-09-19 16:59:18 +0200

    Added remove behaviour and double counter

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,19 +1,26 @@
 <script setup lang="ts">
-    import {ref} from "vue";
+    import {ref, computed} from "vue";
 
     const count = ref(0);
+    const doubleCount = computed(() => count.value * 2);
 
     const add = () => {
         count.value++;
+    }
+
+    const remove = () => {
+        count.value--;
     }
 
 </script>
 
 <template>
     <h3>Your current count is:</h3>
-    <p> {{ count }}</p>
+    <p> Single counter: {{ count }}</p>
+    <p> Souble counter: {{ doubleCount }}</p>
     <div>
         <button @click="add">Add</button>
+        <button @click="remove">Remove</button>
     </div>
 </template>
 
